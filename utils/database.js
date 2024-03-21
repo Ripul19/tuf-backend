0aa239da64b318aec0f98a59140d16e8f4d2da0e
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-22 02:16:06 +0530

    changes

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
 
 let sequelize;
 
@@ -6,6 +7,7 @@ try {
   sequelize = new Sequelize(process.env.DB_NAME_DATABASE, process.env.DB_NAME_USER, process.env.DB_NAME_PASSWORD, {
     host: process.env.DB_NAME_HOST,
     dialect: 'mysql',
+    dialectModule: mysql2,
   });
 } catch (error) {
   console.error('Error connecting to the database:', error);
