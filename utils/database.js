4317b3edd195cf5020fbd3661e31bf4f4cf2db86
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-21 23:39:36 +0530

    database

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME_DATABASE, process.env.DB_NAME_USER, process.env.DB_NAME_PASSWORD, {
   host: process.env.DB_NAME_HOST,
-  dialect: 'mysql'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
