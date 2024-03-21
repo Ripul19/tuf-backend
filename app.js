380a755dc313b3781eddd31baefbd28272a96997
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-22 02:28:07 +0530

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 
 sequelize
-  .sync()
+  .authenticate()
   .then(() => {
     console.log('Database connection has been established successfully.');
   })
