d9202467e54292dd9619c43489e9b05c1f421881
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-22 09:56:39 +0530

    changes

--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,36 @@
+// const Sequelize = require('sequelize');
+// const mysql2 = require('mysql2');
+
+// let sequelize;
+
+// try {
+//   sequelize = new Sequelize(process.env.DB_NAME_DATABASE, process.env.DB_NAME_USER, process.env.DB_NAME_PASSWORD, {
+//     host: process.env.DB_NAME_HOST,
+//     dialect: 'mysql',
+//     dialectModule: mysql2,
+//   });
+// } catch (error) {
+//   console.error('Error connecting to the database:', error);
+// }
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
-const mysql2 = require('mysql2');
+const pg = require('pg');
 
 let sequelize;
 
 try {
   sequelize = new Sequelize(process.env.DB_NAME_DATABASE, process.env.DB_NAME_USER, process.env.DB_NAME_PASSWORD, {
     host: process.env.DB_NAME_HOST,
-    dialect: 'mysql',
-    dialectModule: mysql2,
+    dialect: 'postgres',
+    dialectModule: pg,
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false 
+        }
+      }
   });
 } catch (error) {
   console.error('Error connecting to the database:', error);
