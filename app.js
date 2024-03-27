f0201635c5a31aaa200d3e07fcf12384283a0ea0
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-27 17:21:24 +0530

    revert to before redis

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const sequelize = require('./src/utils/database.js')
-const { initializeRedis } = require('./src/middlewares/redis.js');
 const tableRoutes = require('./src/routes/table.js');
-const initializeRedisClient = require('./src/middlewares/redis.js');
 
 const app = express();
 app.use(cors());
@@ -19,23 +17,11 @@ sequelize
     console.error('Unable to connect to the database:', err);
 });
 
-async function initializeRedisServer() {
-  try{
-    await initializeRedisClient();
-    console.log('Redis Server initialization complete')
-  }
-  catch(error) {
-    console.log('Redis Server initialization error ', error)
-  }
-}
-
 app.use('/api/table', tableRoutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello Get</h1>');
 });
-
-initializeRedisServer();
 
 const PORT = process.env.DB_NAME_PORT || 4000
 
