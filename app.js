4367b58e815e1089f4d0204c416b573fb5e79cee
Ripul Khurana <ripulkhurana77@gmail.com>
2024-03-27 14:14:36 +0530

    redis setup

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 
-const sequelize = require('./utils/database.js')
-
-const tableRoutes = require('./routes/table.js');
+const sequelize = require('./src/utils/database.js')
+const { initializeRedis } = require('./src/middlewares/redis.js');
+const tableRoutes = require('./src/routes/table.js');
+const initializeRedisClient = require('./src/middlewares/redis.js');
 
 const app = express();
 app.use(cors());
@@ -18,11 +19,23 @@ sequelize
     console.error('Unable to connect to the database:', err);
 });
 
+async function initializeRedisServer() {
+  try{
+    await initializeRedisClient();
+    console.log('Redis Server initialization complete')
+  }
+  catch(error) {
+    console.log('Redis Server initialization error ', error)
+  }
+}
+
 app.use('/api/table', tableRoutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello Get</h1>');
 });
+
+initializeRedisServer();
 
 const PORT = process.env.DB_NAME_PORT || 4000
 
---
+++ src/middlewares/redis.js
@@ -0,0 +1,25 @@
+const { createClient } = require('redis');
+let redisClient;
+
+async function initializeRedisClient() {
+  redisClient = createClient({
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT
+        }
+    });
+
+  redisClient.on('connect', () => {
+    console.log('Connected to Redis!');
+  });
+
+  redisClient.on('error', (err) => {
+    console.error('Redis Connection Error:', err);
+  });
+
+  await redisClient.connect(); 
+  console.log('Redis connected successfully!');
+}
+
+module.exports = initializeRedisClient;
